5bf9b1142f1fa351e740dde04d238a94bc334bc5
Isabelle Galleberg <isabellegalleberg@gmail.com>
2023-03-07 11:13:47 +0100

    feat: project setup

---
+++ android/src/com/mygdx/tdt4240/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.tdt4240;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.tdt4240.SamfSurvivor;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SamfSurvivor(), config);
+	}
+}

c1f45acc42593cd6c583ec59673ed8b0674938b1
Juni <junitrigger@gmail.com>
2023-03-14 16:36:16 +0100

    feat: added empty botbehaviorclass

--- core/src/com/mygdx/tdt4240/states/PlayState/Model/ecs/components/BotBehaviorComponent.kt
+++ core/src/com/mygdx/tdt4240/states/PlayState/Model/ecs/components/BotBehaviorComponent.kt
@@ -1,10 +1,10 @@
 package com.mygdx.tdt4240.states.PlayState.Model.ecs.components
 import com.github.quillraven.fleks.Component
 import com.github.quillraven.fleks.ComponentType
+import com.mygdx.tdt4240.states.PlayState.Model.ecs.components.BotBehavior.BotBehavior
 
 data class BotBehaviorComponent(
-    //TODO: create botbehaviorclass and add here
-    //var botBehavior: BotBehavior = BotBehavior
+    var botBehavior: BotBehavior = BotBehavior,
     var life: Int = 5 //remove this
 ) : Component<BotBehaviorComponent> {
 

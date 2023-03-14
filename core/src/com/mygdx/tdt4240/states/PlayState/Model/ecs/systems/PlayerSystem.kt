fcaaa1554fc10aef30b57fc630916996447895de
Juni <junitrigger@gmail.com>
2023-03-14 16:23:13 +0100

    feat: playersystem in ecs

--- core/src/com/mygdx/tdt4240/states/PlayState/Model/ecs/systems/PlayerSystem.kt
+++ core/src/com/mygdx/tdt4240/states/PlayState/Model/ecs/systems/PlayerSystem.kt
@@ -1,4 +1,15 @@
 package com.mygdx.tdt4240.states.PlayState.Model.ecs.systems
 
-class PlayerSystem {
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.IteratingSystem
+import com.github.quillraven.fleks.World.Companion.family
+import com.mygdx.tdt4240.states.PlayState.Model.ecs.components.CharacterComponent
+/* System for the player and NPC*/
+class PlayerSystem : IteratingSystem(
+    family { all(CharacterComponent) }
+) {
+
+    override fun onTickEntity(entity: Entity) {
+        TODO("Funksjon som oppdaterer alle spillerne (f.eks. sprites og posisjon")
+    }
 }

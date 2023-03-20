4a358deb21f06620757ad3b0a4cfc9436cfb570c
Isabelle Galleberg <isabellegalleberg@gmail.com>
2023-03-20 16:54:55 +0100

    feat: firebase setup

--- android/src/com/mygdx/tdt4240/AndroidLauncher.kt
+++ android/src/com/mygdx/tdt4240/AndroidLauncher.kt
@@ -8,6 +8,6 @@ class AndroidLauncher : AndroidApplication() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		val config = AndroidApplicationConfiguration()
-		initialize(Game(), config)
+		initialize(Game(AndroidAPI()), config)
 	}
 }

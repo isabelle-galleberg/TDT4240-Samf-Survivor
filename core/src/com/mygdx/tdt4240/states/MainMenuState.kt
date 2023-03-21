019e695bd983aab9c0f5a74925f2425f691f3eed
CamillaKK <camillakveimkopperud@gmail.com>
2023-03-21 19:09:56 +0100

    feat: add basic buttons

--- core/src/com/mygdx/tdt4240/states/MainMenuState.kt
+++ core/src/com/mygdx/tdt4240/states/MainMenuState.kt
@@ -1,27 +1,100 @@
 package com.mygdx.tdt4240.states
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.mygdx.tdt4240.firebase.API
+import com.mygdx.tdt4240.firebase.User
+import com.mygdx.tdt4240.states.PlayState.Model.PlayState
 
 class MainMenuState(
     stateManager: StateManager
 ) : State(stateManager){
 
-    private val logo = Texture("logo.png")
-    private val titleText = BitmapFont()
+    private val stage = Stage()
+    private val skin = Skin(Gdx.files.internal("skin/uiskin.json"))
+    private val playBtn = TextButton("Play", skin)
+    private val highScoreBtn = TextButton("Highscore", skin)
+    private val tutorialBtn = TextButton("Tutorial", skin)
+    private val logOutBtn = TextButton("Log Out", skin)
+    private val textFieldStyle: TextField.TextFieldStyle = skin.get(TextField.TextFieldStyle::class.java)
 
+    init{
+        playBtn.setSize(1000f,150f)
+        playBtn.setPosition(Gdx.graphics.width.toFloat()/2-playBtn.width/2,Gdx.graphics.height.toFloat()*3/5)
+
+        tutorialBtn.setSize(1000f,150f)
+        tutorialBtn.setPosition(Gdx.graphics.width.toFloat()/2-playBtn.width/2,Gdx.graphics.height.toFloat()*2/5)
+
+        highScoreBtn.setSize(1000f,150f)
+        highScoreBtn.setPosition(Gdx.graphics.width.toFloat()/2-playBtn.width/2,Gdx.graphics.height.toFloat()*1/5)
+
+        logOutBtn.setSize(1000f,150f)
+        logOutBtn.setPosition(Gdx.graphics.width.toFloat()/2-playBtn.width/2,Gdx.graphics.height.toFloat()*0/5)
+
+        textFieldStyle.font.data.setScale(3f)
+        handlePlay()
+        handleTutorial()
+        handleHighScore()
+        handleLogOut()
+        stage.addActor(playBtn)
+        stage.addActor(tutorialBtn)
+        stage.addActor(highScoreBtn)
+        stage.addActor(logOutBtn)
+    }
+
+    private fun handlePlay(){
+        playBtn.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                //stateManager.push(RegisterState(stateManager))
+                println("play button clicked")
+            }
+        })
+    }
+
+    private fun handleTutorial(){
+        tutorialBtn.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                //stateManager.push(RegisterState(stateManager))
+                println("tutorial button clicked")
+            }
+        })
+    }
+
+    private fun handleHighScore(){
+        highScoreBtn.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                //stateManager.push(RegisterState(stateManager))
+                println("high score button clicked")
+            }
+        })
+    }
+
+    private fun handleLogOut(){
+        logOutBtn.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                //stateManager.push(RegisterState(stateManager))
+                println("log out button clicked")
+            }
+        })
+    }
     override fun update(deltaTime: Float) {
-        // TODO
+        Gdx.input.inputProcessor = stage
     }
 
     override fun render(sprites: SpriteBatch) {
-        sprites.begin()
-        titleText.draw(sprites, "This is the main menu", 20f, 200f)
-        sprites.draw(logo, 0f, 300f)
-        sprites.end()
+        stage.act(Gdx.graphics.deltaTime)
+        stage.draw()
     }
 
     override fun dispose() {
-        logo.dispose()
+        stage.dispose()
+        skin.dispose()
     }
 }

c10c6dbc5726664314a9f93a0b09b23acc1c0734
Elise Kittelsby <eliseki@stud.ntnu.no>
2023-03-21 15:43:41 +0100

    feat: created function to build smallButtons-to standardize buttonsize

--- core/src/com/mygdx/tdt4240/sprites/Common.kt
+++ core/src/com/mygdx/tdt4240/sprites/Common.kt
@@ -1,33 +1,39 @@
 package com.mygdx.tdt4240.sprites
 
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.mygdx.tdt4240.utils.Constants
 
 
 class Common {
-    val winWidht=(Constants.GAME_WIDTH/10)*8
-    val winHeight=(Constants.GAME_HEIGHT/8)*6
-    public val logoTexture=Texture("logo.png")
+    val WIN_WIDTH=(Constants.GAME_WIDTH /10)*7
+    val WIN_HEIGHT=(Constants.GAME_HEIGHT /8)*6
+
+    val logoTexture=Texture("logo.png")
+    val samfTexture=Texture("samfundet.png")
+    val windowSprite=createWindow()
     public val titleText = BitmapFont()
-    val winTex=createWindow()
-    val logoSprite = Sprite(logoTexture,Constants.GAME_WIDTH, 80)
 
 
-    public fun createLogo(){
-        val logoSprite = Sprite(logoTexture,Constants.GAME_WIDTH, 100)
-        logoSprite.setPosition(0F, 100F)
-    }
     public fun createWindow(): Sprite {
-        val pixmap = Pixmap(winWidht, winHeight, Pixmap.Format.RGBA8888)
-        pixmap.setColor(128F, 0f, 0f, 0.9f)
-        pixmap.fillRectangle(0, 0, winWidht, winHeight)
+        val pixmap = Pixmap(WIN_WIDTH, WIN_HEIGHT, Pixmap.Format.RGBA8888)
+        pixmap.setColor(128F, 0f, 0f, 1f)
+        pixmap.fillRectangle(0, 0, WIN_WIDTH, WIN_HEIGHT)
         val windowTexture = Texture(pixmap)
         pixmap.dispose()
         return Sprite(windowTexture)
     }
 
+    public fun createSmallButton(buttonName: String): TextButton {
+        val skin = Skin(Gdx.files.internal("skin/uiskin.json"))
+        val button = TextButton(buttonName, skin)
+        button.setSize(150f, 50f)
+        return button
+    }
 }

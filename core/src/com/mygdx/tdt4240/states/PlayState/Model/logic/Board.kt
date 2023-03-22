996a3379c3db9e0c6bd8d148b50dfdc2f1aaaad7
FridaK-sys <frida.karlsen@hotmail.com>
2023-03-22 20:50:07 +0100

    feat: created 10x10 board

--- core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Board.kt
+++ core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Board.kt
@@ -1,16 +1,29 @@
 package com.mygdx.tdt4240.states.PlayState.Model.logic
-
-class Board {
+class Board  {
     private val row = 10
     private val column = 10
 
-    val boardArray = Array(row) { IntArray(column) }
+    val grid = Array(row) { IntArray(column) }
 
+    fun board(arr: Array<IntArray>) {
+        for (i in arr.indices) {
+            for (j in arr[i].indices) {
+                if (i == 0 || j == 0 || i == arr.size -1 || j == arr[i].size -1) {
+                    arr[i][j] = 1
 
+                }
 
-
-
-
-
+                print("${arr[i][j]} ")
+            }
+            println()
+        }
+    }
 
 }
+fun main() {
+    val b = Board()
+    val g = b.grid
+    print(b.board(g))
+
+}
+

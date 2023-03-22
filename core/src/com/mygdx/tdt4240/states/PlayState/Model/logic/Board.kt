d6fca9f1184241b6219f2960d61b3e0191c2823e
FridaK-sys <frida.karlsen@hotmail.com>
2023-03-22 22:46:37 +0100

    feat: created board with entities

--- core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Board.kt
+++ core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Board.kt
@@ -1,29 +1,57 @@
 package com.mygdx.tdt4240.states.PlayState.Model.logic
+
+import com.mygdx.tdt4240.states.PlayState.Model.ecs.entities.*
+
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.World
+import com.github.quillraven.fleks.world
+
+
 class Board  {
     private val row = 10
     private val column = 10
 
-    val grid = Array(row) { IntArray(column) }
+    val grid = Array(10) { arrayOfNulls<Entity>(10) }
 
-    fun board(arr: Array<IntArray>) {
+
+
+    fun initBoard(arr: Array<Array<Entity?>>) {
         for (i in arr.indices) {
             for (j in arr[i].indices) {
-                if (i == 0 || j == 0 || i == arr.size -1 || j == arr[i].size -1) {
-                    arr[i][j] = 1
-
-                }
-
                 print("${arr[i][j]} ")
             }
             println()
         }
     }
 
+    fun drawBoard(arr: Array<Array<Entity?>>, world: World) {
+        for (i in arr.indices) {
+            for (j in arr[i].indices) {
+                //Walls
+                if (i == 0 || j == 0 || i == arr.size -1 || j == arr[i].size -1) {
+                    arr[i][j] = WallFactory.createWall(world, Pair(i,j))
+                }
+                //Crates
+                else
+                     {
+                    arr[i][j] = CrateFactory.createCrate(world, Pair(i,j))
+                }
+            }
+        }
+        //Players
+        arr[1][1] = PlayerFactory.createPlayer(world, Pair(1,1))
+        arr[8][8] = PlayerFactory.createPlayer(world, Pair(8,8))
+
+        initBoard(arr)
+
+    }
+
 }
 fun main() {
     val b = Board()
     val g = b.grid
-    print(b.board(g))
+    val world = world {}
+    print(b.drawBoard(g, world))
 
 }
 
--- core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Player.kt
+++ core/src/com/mygdx/tdt4240/states/PlayState/Model/logic/Player.kt
@@ -1,4 +1,6 @@
 package com.mygdx.tdt4240.states.PlayState.Model.logic
 
 class Player {
+
+
 }

5ef9a186514ad1faa7e34475ec438e02c9cd29ad
Elise Kittelsby <eliseki@stud.ntnu.no>
2023-04-12 16:07:53 +0200

    feat: added functions to buttons

--- core/src/com/mygdx/tdt4240/states/TutorialState.kt
+++ core/src/com/mygdx/tdt4240/states/TutorialState.kt
@@ -5,7 +5,11 @@ import com.mygdx.tdt4240.sprites.Buttons
 
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.mygdx.tdt4240.sprites.BackBtn
+import com.mygdx.tdt4240.sprites.Logo
 import com.mygdx.tdt4240.utils.Constants.GAME_HEIGHT
 import com.mygdx.tdt4240.utils.Constants.GAME_WIDTH
 import com.mygdx.tdt4240.utils.Constants.TUTORIAL_HEIGHT
@@ -18,9 +22,16 @@ import com.mygdx.tdt4240.utils.Constants.TUTORIAL_WIDTH
  */
 
 class TutorialState(stateManager: StateManager) : State(stateManager) {
-    private val logo = Texture("logo.png")
+    private val logo = Logo().createLogo()
     private val background = Texture("samfundet.png")
+
+    private val logo2=Texture("logo.png")
+    private val cat=Texture ("cat1.png")
+    private val imgArray= arrayOf(background,cat,logo2)
+    private var pointer=0
+
     private val stage = Stage()
+    private val backBtn = BackBtn().createBackBtn()
     private val nextButton = Buttons().createSmallButton("Next")
     private val backButton = Buttons().createSmallButton("Back")
     private val pixmap = Pixmap(TUTORIAL_WIDTH, TUTORIAL_HEIGHT, Pixmap.Format.RGBA8888)
@@ -35,17 +46,64 @@ class TutorialState(stateManager: StateManager) : State(stateManager) {
         backButton.setPosition((GAME_WIDTH / 5) - nextButton.width / 2, 20f)
         stage.addActor(nextButton)
         stage.addActor(backButton)
+
+        backButton.isDisabled=true
+
+        handleBackButtonClick()
+        handleNextButtonClick()
+    }
+
+    private fun handleNextButtonClick(){
+        nextButton.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                if (pointer==0)
+                    backButton.isDisabled = false
+                if (pointer<imgArray.size-2) {
+                    pointer += 1
+                    nextButton.isDisabled = false
+                }
+                else if (pointer==imgArray.size-2){
+                    pointer+=1
+                    nextButton.isDisabled = true
+                }
+                else if (pointer>imgArray.size-2)
+                    nextButton.isDisabled = true
+            }
+        })
+    }
+    private fun handleBackButtonClick(){
+        backButton.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                if(pointer==imgArray.size-1)
+                    nextButton.isDisabled = false
+                if (pointer>1){
+                    pointer-=1
+                    backButton.isDisabled = false
+                }
+                else if (pointer==1){
+                    pointer-=1
+                    backButton.isDisabled = true
+                }
+                else if (pointer<1){
+                    backButton.isDisabled = true
+                }
+            } })
     }
 
     override fun update(deltaTime: Float) {
         Gdx.input.inputProcessor = stage
+        if (BackBtn().backBtnPressed()) {
+            stateManager.push(MainMenuState(stateManager))
+        }
     }
 
     override fun render(sprites: SpriteBatch) {
         sprites.begin()
         sprites.draw(background,0f,0f,GAME_WIDTH,GAME_HEIGHT)
-        sprites.draw(logo,GAME_WIDTH/2-logo.width/2,GAME_HEIGHT-logo.height)
+        logo.draw(sprites)
         sprites.draw(tutorialWindow,GAME_WIDTH/2-tutorialWindow.width/2,0f)
+        sprites.draw(imgArray[pointer], 0f,0f)
+        backBtn.draw(sprites)
         sprites.end()
         stage.act(Gdx.graphics.deltaTime)
         stage.draw()
@@ -53,7 +111,6 @@ class TutorialState(stateManager: StateManager) : State(stateManager) {
 
     override fun dispose() {
         stage.dispose()
-        logo.dispose()
         background.dispose()
         pixmap.dispose()
         tutorialWindow.dispose()

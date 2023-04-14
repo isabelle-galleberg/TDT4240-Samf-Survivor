29806ba94c9305eb59595dd3a28bc2f242ab5301
CamillaKK <camillakveimkopperud@gmail.com>
2023-04-14 11:35:54 +0200

    feat: add pause window

--- core/src/com/mygdx/tdt4240/sprites/Window.kt
+++ core/src/com/mygdx/tdt4240/sprites/Window.kt
@@ -12,10 +12,22 @@ class Window {
     val WIN_WIDTH=(GAME_WIDTH /10).toInt()*7
     val WIN_HEIGHT=(GAME_HEIGHT /8).toInt()*6
 
-    public fun createWindow(): Sprite {
+    val WIN_PAUSE_HEIGHT=(GAME_HEIGHT /7).toInt()*5
+
+    fun createWindow(): Sprite {
         val pixmap = Pixmap(WIN_WIDTH, WIN_HEIGHT, Pixmap.Format.RGBA8888)
-        pixmap.setColor(128F, 0f, 0f, 1f)
+        pixmap.setColor(1F, 1f, 1f, 1f)
         pixmap.fillRectangle(0, 0, WIN_WIDTH, WIN_HEIGHT)
+        val winTex=Texture(pixmap)
+        val winSprite=Sprite(winTex)
+        winSprite.setPosition((GAME_WIDTH-WIN_WIDTH)/2, 0f)
+        return winSprite
+    }
+
+    fun createPauseWindow(): Sprite {
+        val pixmap = Pixmap(WIN_WIDTH, WIN_HEIGHT, Pixmap.Format.RGBA8888)
+        pixmap.setColor(1F, 1f, 1f, 1f)
+        pixmap.fillRectangle(0, 0, WIN_WIDTH, WIN_PAUSE_HEIGHT)
         val winTex=Texture(pixmap)
         val winSprite=Sprite(winTex)
         winSprite.setPosition((GAME_WIDTH-WIN_WIDTH)/2, 0f)

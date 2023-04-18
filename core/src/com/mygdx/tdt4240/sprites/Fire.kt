dc57c0484ef10381103d2d248162122c3056c148
FridaK-sys <frida.karlsen@hotmail.com>
2023-04-18 09:26:26 +0200

    fix: fixed scaling on fire

--- core/src/com/mygdx/tdt4240/sprites/Fire.kt
+++ core/src/com/mygdx/tdt4240/sprites/Fire.kt
@@ -6,7 +6,7 @@ import com.mygdx.tdt4240.utils.Constants
 
 class Fire {
 
-    private val fireSize = Constants.GAME_HEIGHT * 0.1f
+    private val fireSize = Constants.GAME_HEIGHT * 0.3f
     private var fireX = Constants.GAME_WIDTH * 0.5f + Constants.GAME_HEIGHT * 0.35f
     private var fireY = Constants.GAME_HEIGHT * 0.05f
     fun createFire(): Sprite {

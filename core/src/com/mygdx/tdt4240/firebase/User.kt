adf141f630e7e8676c4d3df71aea68871e0395da
FridaK-sys <frida.karlsen@hotmail.com>
2023-04-18 09:35:39 +0200

    fix: fixed merge conflict in User class

--- core/src/com/mygdx/tdt4240/firebase/User.kt
+++ core/src/com/mygdx/tdt4240/firebase/User.kt
@@ -1,18 +1,18 @@
 package com.mygdx.tdt4240.firebase
 
-class User(val username: String, val password: String, val score: Int ) : Comparable<User> {
+class User(val username: String, val password: String, val highscore: Int ) : Comparable<User> {
 
     // Firebase requires an empty constructor
     constructor() : this("", "", 0)
 
     override fun toString(): String {
-        return "$username: $score"
+        return "$username: $highscore"
     }
 
     override fun compareTo(other: User): Int {
-        if (this.score > other.score) {
+        if (this.highscore > other.highscore) {
             return -1
-        } else if (this.score < other.score) {
+        } else if (this.highscore < other.highscore) {
             return 1
         }
         return username.compareTo(other.username)
